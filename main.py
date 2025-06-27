1b7feef3e7ed580b18442d857bf994668cab9c63
Kirtan Dave <66071191+dave-kirtan@users.noreply.github.com>
2025-06-27 21:19:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# import time
+
 
 from bs4 import BeautifulSoup
 import requests
